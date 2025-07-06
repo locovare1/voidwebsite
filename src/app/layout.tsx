3fbff83cab41199806520284b4c3cbb7942450f8
locovare1 <squidscents@outlook.com>
2025-07-06 15:11:50 -0400

    updated all the assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>
+          Void Esports
+        </title>
+      </head>
       <body className={inter.className}>
         <Navbar />
         <main className="min-h-screen">{children}</main>
