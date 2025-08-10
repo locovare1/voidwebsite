04b58995a1ad2f8c9cf61d1fe1dc9a9434fcdfbd
locovare1 <squidscents@outlook.com>
2025-08-10 09:36:54 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <body className={inter.className}>
         <Navbar />
         <main className="min-h-screen">{children}</main>
