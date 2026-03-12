6c4a5dba7f914f4356b515beb5f714413a4b84d6
Aayan Bin Asim <aayan.b.asim@gmail.com>
2026-03-12 21:54:51 +0300

    refactor: remove redundant loading spinner and optimize global CSS

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -11,7 +11,6 @@ export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as CartIcon } from './CartIcon';
 export { default as Badge } from './Badge';
-export { default as LoadingSpinner } from './LoadingSpinner';
 export { default as LoadingScreen } from './LoadingScreen';
 export { default as ScrollToTop } from './ScrollToTop';
 export { default as SectionHeader } from './SectionHeader';
